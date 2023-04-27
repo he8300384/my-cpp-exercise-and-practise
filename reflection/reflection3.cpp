2a86d37ac989d25047ca69edd7dea58fadb510ba
he8300384 <he8300384@163.com>
2023-04-28 01:35:13 +0800

    test reflect

--- reflection/reflection3.cpp
+++ reflection/reflection3.cpp
@@ -5,6 +5,15 @@
 属性操作函数 在类外定义 使用基类指针转换为子类指针 调用具体子类属性
 操作函数的指针列表收录在基类vector组内 基类有统一遍历调用数组内函数的方法
 
+优势：
+    逻辑实现简单
+    对与只需要批量 序列化 属性的情况够用
+
+劣势：
+    侵入式
+    属性操作是批量的无法单独操控 
+    基类 辅助添加类 操作函数 改写量不小
+
 */
 #include<iostream>
 #include<sstream>
@@ -57,7 +66,7 @@ std::stringstream s; //通过流写入属性值
     AutoAdd __auto__add_##data{this, WRITE(data), READ(data)} //当宏被插入任意继承了反射父类的子类中时 它就搜集this指针为子类添加变量的操作函数
 
 //1111 普通类继承 反射用基类 并用宏注册字段属性
-class RefectClass:public BaseRefect 
+class RefectClass : public BaseRefect 
 {
     REFLECT(int, a);
     REFLECT(std::string, name);
@@ -67,8 +76,13 @@ class RefectClass:public BaseRefect
 int main()
 {
     RefectClass a;
-    s << 22 << "heqin" << 11;
-    a.doRead();
+    s << 22 << "heqin\n" << 11; //使用sstream流写数据
+    a.doRead();                 //使用sstream流写数据
+    a.doWrite();    
+
+    s.clear();  //清理流
+    s << 33 << "heqin2\n" << 44;//改写数据
+    a.doRead();                 //改写数据
     a.doWrite(); 
     
 }
